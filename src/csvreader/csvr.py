9b6ed92b892f4b07fea34b46b99c705d3ee41217
Ravi <Ravi@Work>
2015-10-29 16:50:02 -0700

    added dt profilin

--- src/csvreader/csvr.py
+++ src/csvreader/csvr.py
@@ -13,7 +13,8 @@ def csvread()->list:
         next(csvfile)
         next(csvfile)
         for row in line:
-           age = round(float(row[4] if row[4]!='' else -1))
-           fare = round(float(row[7] if row[7]!='' else -1))
-           passList.append(Pass(int(row[0]),bool(row[1]),int(row[2]),bool(row[3]),age,int(row[5]),int(row[6]),fare))
+            age = round(float(row[4] if row[4]!='' else -1))
+            fare = round(float(row[7] if row[7]!='' else -1))
+            sex = True if row[3] == 'male' else False
+            passList.append(Pass(int(row[0]),bool(int(row[1])),int(row[2]),sex,age,int(row[5]),int(row[6]),fare))
         return passList
--- src/csvreader/explor.py
+++ src/csvreader/explor.py
@@ -6,24 +6,79 @@ Created on Oct 28, 2015
 import csvr
 import math
 import statistics as stats
-anaProps = 'age'.split(sep = ' ')
+from collections import namedtuple
+anaProps = 'age fare'.split(sep = ' ')
 discreteProps = 'pclass '
 passList = csvr.csvread()
 ageMedian = stats.median([x.age for x in passList])
-def getBasicData()->list:
+treePass = namedtuple('treePass','sex age pclass live dead')
+tPass = [treePass(x,y,z,0,0) for x in [True,False] for y in range(1,6) for z in range(1,4)]
+def getBasicData(passList)->list:
     propVals = []
     for traits in anaProps:
         propVals.append([traits,
-                         round(stats.mean([eval('x.'+traits) for x in passList if x!=-1]),3),
-                         round(stats.median([eval('x.'+traits) for x in passList]),3),
-                         round(stats.mode([eval('x.'+traits) for x in passList]),3),
-                         sum(1 for x in passList if x!=-1)]
+                         round(stats.mean([eval('x.'+traits) for x in passList if eval('x.'+traits)!=-1]),3),
+                         round(stats.median([eval('x.'+traits) for x in passList  if eval('x.'+traits)!=-1]),3),
+                         round(stats.mode([eval('x.'+traits) for x in passList if eval('x.'+traits)!=-1]),3),
+                         sum(1 for x in passList if eval('x.'+traits)!='-1')]
                          
                         
                         )
     return propVals
 
-for i in getBasicData():
-    print(i[0]+' mean= '+str(i[1])+' median= '+str(i[2])+' mode= '+str(i[3]))
+props = getBasicData(passList)
 
+def RemoveBlanks():
+    for r in range(0,len(passList)):
+        if passList[r].age == -1:
+            passList[r]=passList[r]._replace(age = props[0][3])
+RemoveBlanks()
+passSurv = [x for x in passList if x.live == True]
+passDead = [x for x in passList if x.live == False]
+def gender():  
+    m = 0
+    f=0
+    for i in passSurv:
+        if i.sex:
+            m+=1
+        else:
+            f+=1
+    print(m/(m+f))
+    print(f/(m+f))        
+      
+    
+    m = 0
+    f=0
+    for i in passDead:
+        if i.sex:
+            m+=1
+        else:
+            f+=1
+    print(m/(m+f))
+    print(f/(m+f))  
+def ageBuckets(age):
+
+    if age<=5:
+        return 1
+    elif age>5 and age<16:
+        return 2
+    elif age>=16 and age<25:
+        return 3
+    elif age >=25 and age <60:
+        return 4
+    elif age>= 60:
+        return 5
+def incNode(age,sex,pclass,surv):
+    for i in range(0,len(tPass)):
+        if tPass[i].age == age and tPass[i].sex == sex and tPass[i].pclass == pclass:
+            
+            if surv:
+                tPass[i]=tPass[i]._replace(live = tPass[i].live+1)
+            else:
+                tPass[i]=tPass[i]._replace(dead = tPass[i].dead+1)
+            break
+def genTree(): 
+    for p in passList:
+        incNode(ageBuckets(p.age),p.sex,p.pclass,p.live)
+genTree()
 
